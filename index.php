e1c0b30f88120b1ba87d74182f07fbc297ff21c7
fatih emre akyüz <fatihemre505@gmail.com>
2022-09-21 20:47:57 +0000

    mt

--- index.php
+++ index.php
@@ -9,11 +9,10 @@
     <title>PHP || Functions </title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    
 </head>
 
 <body>
-    <div class="container mt-2">
+    <div class="container mt-3">
         <div class="row">
             <div class="col">
                 <div class="card bg-light">
